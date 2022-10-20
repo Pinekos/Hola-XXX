7e471b39103bb3059b170768f9f8e562f60db886
Pilar <113586357+Pinekos@users.noreply.github.com>
2022-10-20 09:33:54 +0200

    Add files via upload

---
+++ holaa.cs
@@ -0,0 +1,15 @@
+﻿//C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe
+
+using System;
+
+namespace holaa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Hola " +nombre);
+        }
+    }
+}
